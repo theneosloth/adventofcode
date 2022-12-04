22344da4bf6d4cf4943e378c800b2c302d9f80fd
Stefan Kuznetsov <neosloth@posteo.net>
2022-12-03 17:11:58 -0800

    Day 3

--- day1/main.py
+++ day1/main.py
@@ -8,8 +8,7 @@ Result = namedtuple("Result", "top_elf top_three_elves")
 def get_calories(input: str) -> Result:
     # Returns the list like [['1', 2'], ['1'], ['3', '4']]
     groups = [group.split() for group in input.split("\n\n")]
-    int_groups = starmap(lambda *list: map(int, list), groups)
-    sums = sorted(map(sum, int_groups))
+    sums = sorted([sum([int(i) for i in x]) for x in groups])
     top_elf = sums[-1]
     top_three_elves = sum(sums[-3:])
     return Result(top_elf, top_three_elves)
--- day3/main.py
+++ day3/main.py
@@ -1,27 +1,40 @@
 import fileinput
-from itertools import chain
-def get_priority(char: str):
+from functools import reduce
+from itertools import chain, islice
+
+priority = int
+
+
+def get_priority(char: str) -> priority:
     if len(char) > 1:
         raise TypeError("Character expected")
 
-    if ord(char) >= ord('a') and ord(char) <= ord('z'):
-        return ord(char) - ord('a') + 1
+    if ord(char) >= ord("a") and ord(char) <= ord("z"):
+        return ord(char) - ord("a") + 1
 
-    if ord(char) >= ord('A') and ord(char) <= ord('Z'):
-        return ord(char) - ord('A') + 27
+    if ord(char) >= ord("A") and ord(char) <= ord("Z"):
+        return ord(char) - ord("A") + 27
 
     raise IndexError("Provided character not in the alphabet")
 
-def split_rucksack(lines: str):
-    compartments = [
-        (l[:len(l)//2], l[len(l)//2:]) for l in lines
-    ]
-    overlaps = chain(*[
-        (set(l) & set(r)) for l,r in compartments
-    ])
+
+def split_rucksack(lines: list[str]) -> int:
+    compartments = [(l[: len(l) // 2], l[len(l) // 2 :]) for l in lines]
+    overlaps = chain.from_iterable([(set(l) & set(r)) for l, r in compartments])
+
     return sum([get_priority(i) for i in overlaps])
+
+
+def get_badge(lines: list[str]) -> int:
+    chunks = zip(*[iter(lines)] * 3)
+    items = [[set(c.strip()) for c in group] for group in chunks]
+    badges = [reduce(lambda a, b: a & b, i) for i in items]
+    priorities = [get_priority(list(i)[0]) for i in badges]
+    return sum(priorities)
+
 
 if __name__ == "__main__":
     with fileinput.FileInput() as f:
         lines = list(f)
         print(split_rucksack(lines))
+        print(get_badge(lines))
